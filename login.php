7975d43ce91cfcad9fcb74e32b2cb716464346fd
Phyyyyyyyyyyyy <itzdroidplayz@gmail.com>
2026-07-27 14:49:27 +0800

    prior commit

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Storage Management System</title>
+
+    <link rel="stylesheet" href="assets/css/login.css">
+
+</head>
+
+<body>
+
+<div class="login-container">
+
+    <h1>Storage Management System</h1>
+
+    <form>
+
+        <input
+            type="text"
+            placeholder="Username"
+        >
+
+        <input
+            type="password"
+            placeholder="Password"
+        >
+
+        <button>
+
+            Login
+
+        </button>
+
+    </form>
+
+</div>
+
+</body>
+</html>
